6007440f28295cb50f08dd3d2bdd0aaee3ba6ccb
danxiyang <664629957@qq.com>
2017-12-01 10:48:16 +0800

    substring模板

---
+++ subString Template/demo1_miniumWindow.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s, t):
+        counter = len(t)
+        min_dis = counter + 1
+        start = 0
+        window_start = 0
+        need = {}
+
+
+        for char in t:
+            need[char] = need.get(char, 0) + 1
+
+        for end, val in enumerate(s):
+            if s[end] in need:
+                if need[val] > 0:
+                    counter -= 1
+                need[val] -= 1
+
+            while not counter:
+                if s[start] in need:
+                    need[s[start]] += 1
+                if need[s[start]] > 0:
+                    counter += 1
+
+                start += 1
+
+            if min_dis > end - start:
+                min_dis = end - start + 1
+                window_start = start - 1
+        return "" if min_dis == len(t) else s[window_start: window_start + min_dis + 1]
