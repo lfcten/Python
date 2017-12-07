d0c9184d64eef043e5afd017c9e2daa59a04dba6
danxiyang <664629957@qq.com>
2017-12-07 15:17:59 +0800

    verticalOrder

---
+++ Binary Tree Vertical Order Traversal/Binary Tree Vertical Order Traversal.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+
+class Solution:
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return None
+        self.res = [[root.val]]
+        self.pos = 0
+        self.helper(root)
+        return self.res
+
+    def helper(self, node):
+        if node.left:
+            if self.pos == 0:
+                self.res.insert(self.pos, [node.left.val])
+            else:
+                self.res[self.pos - 1].append(node.left.val)
+                self.pos -= 1
+            self.helper(node.left)
+
+        if node.right:
+            print(self.res, self.pos)
+            if self.pos + 1 == len(self.res):
+                self.res.insert(len(self.res), [node.right.val])
+                self.pos += 1
+            else:
+                self.res[self.pos + 1].append(node.right.val)
+            self.helper(node.right)
+        else:
+            self.pos += 1
---
+++ lru/lru_cache.py
@@ -0,0 +1,33 @@
+import collections
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key)
+        self.dic[key] = v
+        return v
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.dic:
+            self.dic.pop(key)
+        elif self.remain > 0:
+            self.remain -= 1
+        else:
+            self.dic.popitem(last=False)
+        self.dic[key] = value
